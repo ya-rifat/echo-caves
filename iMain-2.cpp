aebecef053bfdc3179411bd3a9d2c85a54645634
YA-Rifat <yrifat5201@gmail.com>
2025-06-02 16:10:44 +0600

    start_screen

--- iMain-2.cpp
+++ iMain-2.cpp
@@ -1,31 +1,25 @@
 #include "iGraphics.h"
 
+//function declarations
+void startScreen();
+
+//button states
 int play_button_state;
 
-int currentScreen = 0; //0 means star screen
+int currentScreen; //0 means star screen
 
 void iDraw()
 {
-    iClear();
+	iClear();
 
-		//start_screen
-		iShowImage(177, 385, "assets/title_logo.png");
-
-		switch (play_button_state) {
-			case 0:
-				iShowImage(253, 280, "assets/play_button.png");
-				break;
-			
-			case 1:
-				iShowImage(253, 280, "assets/play_button_hover.png");
-				break;
-
-			case 2:
-				iShowImage(253, 280, "assets/play_button_pressed.png");
-				break;
-			default:
-				break;
-		}
+	switch (currentScreen) {
+		case 0:
+			startScreen();
+			break;
+		
+		default:
+			break;
+	}
 }
 
 //mouse move
@@ -45,18 +39,22 @@ function iMouse() is called when the user presses/releases the mouse.
 */
 void iMouse(int button, int state, int mx, int my)
 {
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-    {
-      if (play_button_state) play_button_state = 2;
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+			switch (currentScreen) {
+				case 0:
+					if (play_button_state) play_button_state = 2;
+					break;
+				
+				default:
+					break;
+			}
     }
-    if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
-    {
+    if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
         // place your codes here
     }
 }
 
 /*
-function iMouseWheel() is called when the user scrolls the mouse wheel.
 dir = 1 for up, -1 for down.
 */
 void iMouseWheel(int dir, int mx, int my)
@@ -64,49 +62,70 @@ void iMouseWheel(int dir, int mx, int my)
     // place your code here
 }
 
-/*
-function iKeyboard() is called whenever the user hits a key in keyboard.
-key- holds the ASCII value of the key pressed.
-*/
 void iKeyboard(unsigned char key)
 {
-    switch (key)
-    {
+	switch (key) {
     case 'q':
-        // do something with 'q'
-        break;
-    // place your codes for other keys here
+			// do something with 'q'
+			break;
+			
     default:
-        break;
+			break;
     }
 }
 
 /*
-function iSpecialKeyboard() is called whenver user hits special keys likefunction
-keys, home, end, pg up, pg down, arraows etc. you have to use
-appropriate constants to detect them. A list is:
 GLUT_KEY_F1, GLUT_KEY_F2, GLUT_KEY_F3, GLUT_KEY_F4, GLUT_KEY_F5, GLUT_KEY_F6,
 GLUT_KEY_F7, GLUT_KEY_F8, GLUT_KEY_F9, GLUT_KEY_F10, GLUT_KEY_F11,
 GLUT_KEY_F12, GLUT_KEY_LEFT, GLUT_KEY_UP, GLUT_KEY_RIGHT, GLUT_KEY_DOWN,
 GLUT_KEY_PAGE_UP, GLUT_KEY_PAGE_DOWN, GLUT_KEY_HOME, GLUT_KEY_END,
-GLUT_KEY_INSERT */
+GLUT_KEY_INSERT
+*/
 void iSpecialKeyboard(unsigned char key)
 {
-    switch (key)
-    {
-    case GLUT_KEY_END:
-        // do something
-        break;
-    // place your codes for other keys here
-    default:
-        break;
-    }
+	switch (key) {
+		case GLUT_KEY_END:
+			// do something
+			break;
+
+		default:
+			break;
+	}
 }
 
 int main(int argc, char *argv[])
 {
-    glutInit(&argc, argv);
-    // place your own initialization codes here.
-    iInitialize(800, 500, "Echo Caves");
-    return 0;
+	glutInit(&argc, argv);
+	currentScreen = 0;
+	iInitialize(800, 500, "Echo Caves");
+	return 0;
+}
+
+
+
+/**
+ * Define screens here..........
+ * 
+ * Declare them on top first...
+ */
+
+
+void startScreen() {
+	iShowImage(177, 385, "assets/title_logo.png");
+
+	switch (play_button_state) {
+		case 0:
+			iShowImage(253, 280, "assets/play_button.png");
+			break;
+		
+		case 1:
+			iShowImage(253, 280, "assets/play_button_hover.png");
+			break;
+
+		case 2:
+			iShowImage(253, 280, "assets/play_button_pressed.png");
+			break;
+		default:
+			break;
+	}
 }
