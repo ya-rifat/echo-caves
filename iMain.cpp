068b2123e7bb892a248407ac34d091004865bafd
YA-Rifat <yrifat5201@gmail.com>
2025-06-05 12:44:14 +0600

    a

--- iMain.cpp
+++ iMain.cpp
@@ -7,14 +7,17 @@ void storyScreen();
 void underConstruction();
 
 //button states
-//start screen
-int play_button, settings_button, leaderboard_button, exit_button, help_button;
+//start screen - 0
+int play_button, settings_button, leaderboard_button, exit_button, help_button, story_button;
+
+//story screen - 4
+int back_button_story;
 
 int current_screen; //0 means star screen
 
 //sound channels
 //start screen
-int start_button_sound;
+int button_sound;
 
 void iDraw()
 {
@@ -42,11 +45,15 @@ void iDraw()
 void iMouseMove(int mx, int my) {
 	switch (current_screen) {
 		case 0:		
-			if ((mx > 265 && mx < 535) && (my > 260 && my < 320)) play_button = 1; else play_button = 0;
-			if ((mx > 265 && mx < 535) && (my > 198 && my < 258)) settings_button = 1; else settings_button = 0;
-			if ((mx > 265 && mx < 535) && (my > 126 && my < 196)) leaderboard_button = 1; else leaderboard_button = 0;
+			if ((mx > 100 && mx < 370) && (my > 304 && my < 364)) settings_button = 1; else settings_button = 0;
+			if ((mx > 100 && mx < 370) && (my > 242 && my < 302)) leaderboard_button = 1; else leaderboard_button = 0;
+			if ((mx > 100 && mx < 370) && (my > 180 && my < 240)) story_button = 1; else story_button = 0;
 			if ((mx > 830 && mx < 890) && (my > 10 && my < 70)) exit_button = 1; else exit_button = 0;
 			if ((mx > 765 && mx < 825) && (my > 10 && my < 70)) help_button = 1; else help_button = 0;
+			break;
+
+		case 4:
+			if ((mx > 395 && mx < 497) && (my > 75 && my < 100)) back_button_story = 1; else back_button_story = 0;
 			break;
 
 		default:
@@ -69,10 +76,15 @@ void iMouse(int button, int state, int mx, int my)
 			switch (current_screen) {
 				case 0:
 					if (play_button) play_button = 2;
+					if (story_button) story_button = 2;
 					if (settings_button) settings_button = 2;
 					if (leaderboard_button) leaderboard_button = 2;
 					if (exit_button) exit_button = 2;
 					if (help_button) help_button = 2;
+					break;
+
+				case 4:
+					if (back_button_story) back_button_story = 2;
 					break;
 
 				case 100:
@@ -85,22 +97,40 @@ void iMouse(int button, int state, int mx, int my)
 		if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
 			switch (current_screen) {
 				case 0:
-					if (play_button) play_button = 2;
+					if (play_button) {
+						play_button = 0;
+						button_sound = 0;
+					}
 					if (settings_button) {
 						current_screen = 100;
 						settings_button = 0;
+						button_sound = 0;
 					}
 					if (leaderboard_button) {
 						current_screen = 100;
 						leaderboard_button = 0;
+						button_sound = 0;
+					}
+					if (story_button) {
+						current_screen = 4;
+						story_button = 0;
+						button_sound = 0;
 					}
 					if (exit_button) exit(0);
 					if (help_button) {
-						current_screen = 4;
+						current_screen = 0;
 						help_button = 0;
-						start_button_sound = 0;
+						button_sound = 0;
 					}
 					
+					break;
+
+				case 4:
+					if (back_button_story) {
+						current_screen = 0;
+						back_button_story = 0;
+						button_sound = 0;
+					}
 					break;
 
 				case 100:
@@ -173,46 +203,74 @@ int main(int argc, char *argv[])
 
 
 void startScreen() { //screen index 0
-	iShowImage(218, 475, "assets/title.png");
+	iShowImage(218, 475, "assets/logo-2.png");
 	iShowImage(10, 10, "assets/texts/version.png");
 
-	switch (play_button) {
-		case 0: iShowImage(265, 260, "assets/buttons/play_button.png"); break;
-		case 1: iShowImage(265, 260, "assets/buttons/play_button_hover.png"); break;
-		case 2: iShowImage(265, 260, "assets/buttons/play_button_pressed.png"); break;
-	}
+	// switch (play_button) {
+	// 	case 0: iShowImage(100, 304, "assets/buttons/play_button.png"); break;
+	// 	case 1: iShowImage(100, 304, "assets/buttons/play_button_hover.png"); break;
+	// 	case 2: iShowImage(100, 304, "assets/buttons/play_button_pressed.png");
+	// 		if (!button_sound) iPlaySound("assets/sounds/button_click.wav", false, 80);
+	// 		button_sound = 1;
+	// 		break;
+	// }
 
 	switch (settings_button) {
-		case 0: iShowImage(265, 198, "assets/buttons/settings_button.png"); break;
-		case 1: iShowImage(265, 198, "assets/buttons/settings_button_hover.png"); break;
-		case 2: iShowImage(265, 198, "assets/buttons/settings_button_pressed.png"); break;
+		case 0: iShowImage(100, 304, "assets/buttons/settings_button.png"); break;
+		case 1: iShowImage(100, 304, "assets/buttons/settings_button_hover.png"); break;
+		case 2: iShowImage(100, 304, "assets/buttons/settings_button_pressed.png");
+			if (!button_sound) iPlaySound("assets/sounds/button_click.wav", false, 80);
+			button_sound = 1;
+			break;
 	}
 
 	switch (leaderboard_button) {
-		case 0: iShowImage(265, 136, "assets/buttons/leaderboard_button.png"); break;
-		case 1: iShowImage(265, 136, "assets/buttons/leaderboard_button_hover.png"); break;
-		case 2: iShowImage(265, 136, "assets/buttons/leaderboard_button_pressed.png"); break;
+		case 0: iShowImage(100, 242, "assets/buttons/leaderboard_button.png"); break;
+		case 1: iShowImage(100, 242, "assets/buttons/leaderboard_button_hover.png"); break;
+		case 2: iShowImage(100, 242, "assets/buttons/leaderboard_button_pressed.png");
+			if (!button_sound) iPlaySound("assets/sounds/button_click.wav", false, 80);
+			button_sound = 1;
+			break;
+	}
+
+	switch (story_button) {
+		case 0: iShowImage(100, 180, "assets/buttons/story_button.png"); break;
+		case 1: iShowImage(100, 180, "assets/buttons/story_button_hover.png"); break;
+		case 2: iShowImage(100, 180, "assets/buttons/story_button_pressed.png");
+			if (!button_sound) iPlaySound("assets/sounds/button_click.wav", false, 80);
+			button_sound = 1;
+			break;
 	}
 
 	switch (exit_button) {
 		case 0: iShowImage(830, 10, "assets/buttons/exit_button.png"); break;
 		case 1: iShowImage(830, 10, "assets/buttons/exit_button_hover.png"); break;
-		case 2: iShowImage(830, 10, "assets/buttons/exit_button_pressed.png"); break;
+		case 2: iShowImage(830, 10, "assets/buttons/exit_button_pressed.png");
+			if (!button_sound) iPlaySound("assets/sounds/button_click.wav", false, 80);
+			button_sound = 1;
+			break;
 	}
 	
 	switch (help_button) {
 		case 0: iShowImage(765, 10, "assets/buttons/help_button.png"); break;
 		case 1: iShowImage(765, 10, "assets/buttons/help_button_hover.png"); break;
 		case 2: iShowImage(765, 10, "assets/buttons/help_button_pressed.png");
-			if (!start_button_sound) iPlaySound("assets/sounds/button_click.wav", false, 80);
-			start_button_sound = 1;
+			if (!button_sound) iPlaySound("assets/sounds/button_click.wav", false, 80);
+			button_sound = 1;
 			break;
 	}
 }
 
 void storyScreen() { //screen index 4
 	iShowImage(0, 0, "assets/backgrounds/story_bg.png");
-	iShowImage(400, 80, "assets/texts/back.png");
+	switch (back_button_story) {
+		case 0:
+		case 1: iShowImage(400, 80, "assets/texts/back.png"); break;
+		case 2: iShowImage(400, 80, "assets/texts/back.png");
+			if (!button_sound) iPlaySound("assets/sounds/button_click.wav", false, 80);
+			button_sound = 1;
+			break;
+	}
 	iShowImage(98, 200, "assets/texts/story.png");
 }
 
